f20fcdfd541915254ef7a1980ae7bfa191e21262
kishanmpatel <31142898+kishanmpatel@users.noreply.github.com>
2018-02-05 19:47:00 -0700

    Initial Commit
    Find out how many times character repeats in a word and sort it in ascending order.
    Input: hello
    Output: e1h1l201

---
+++ FindHowManyTimesCharacterInWord.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2DArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any word");
+            string UserInput = Console.ReadLine();
+            string OutputString = "";
+
+            List<string> OutputArray = new List<string>();
+
+            for (int i = 0; i < UserInput.Length; i++)
+            {
+                int count = 0;
+                for(int j = 0; j < UserInput.Length; j++)
+                {
+                    if(UserInput[i] == UserInput[j])
+                    {
+                        count++;
+                    }
+                }
+
+                string result = UserInput[i] + count.ToString();
+                if (!OutputArray.Contains(result))
+                {
+                    OutputArray.Add(UserInput[i] + count.ToString());
+                }
+            }
+
+            OutputArray.Sort();
+
+            foreach(string item in OutputArray)
+            {
+                OutputString += item;
+            }
+
+            Console.WriteLine("{0}", OutputString.ToString());
+            
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
